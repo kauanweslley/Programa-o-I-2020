ccc297be40108b50397e5f22a4e6aeda0321ac6f
kauanweslley <50590816+kauanweslley@users.noreply.github.com>
2020-06-19 01:24:45 -0300

    Botões que alteram as cores da página
    fazer uma página HTML que tenha quatro (4) botões, com os seguintes textos: "Azul", "Amarelo", "Verde", "Vermelho". Ao clicar em cada um desses botões, a cor de fundo da página HTML deve alterar, de acordo com o botão escolhido.
    
    Note que as cores de background devem ser informadas em inglês. Dessa forma:
    - Azul --> blue
    - Amarelo --> yellow
    - Verde --> green
    - Vermelho --> red

---
+++ cores-dom3.js
@@ -0,0 +1,19 @@
+function alterarcor1() {
+    const cores = ["blue"]
+    document.body.style.background = cores
+}
+
+function alterarcor2() {
+    const cores = ["yellow"]
+    document.body.style.background = cores
+}
+
+function alterarcor3() {
+    const cores = ["green"]
+    document.body.style.background = cores
+}
+
+function alterarcor4() {
+    const cores = ["red"]
+    document.body.style.background = cores
+}
